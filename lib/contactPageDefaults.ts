1cd492436d9d46c0a70d74162a8bda0c900a79d2
coteli <avsarcoteli@gmail.com>
2021-10-14 15:54:34 +0300

    first commit

---
+++ lib/contactPageDefaults.ts
@@ -0,0 +1,30 @@
+import { GhostPostOrPage } from "./ghost"
+import { ServiceConfig } from '@components/contact/ContactForm'
+
+export interface ContactPage extends GhostPostOrPage {
+  form_topics: string[]
+  serviceConfig: ServiceConfig
+}
+
+export const defaultPage: ContactPage = {
+  id: 'custom-page-contact',
+  slug: 'contact',
+  url: '/contact',
+  title: 'İletişime Geçin',
+  custom_excerpt: 'Görüş, öneri veya sorularınız için bizimle iletişime geçebilirsiniz!',
+  form_topics: ['Görüş ve önerim var!', 'Soru sormak istiyorum!'],
+  meta_title: 'Contact Us',
+  meta_description: 'A contact form page.',
+  html: '',
+  serviceConfig: {
+    url: '/api/v1/contact',
+    contentType: 'application/json',
+  },
+  featureImage: {
+    url: 'https://static.gotsby.org/v1/assets/images/gatsby-ghost-contact.png',
+    dimensions: {
+      width: 1040,
+      height: 250
+    }
+  }
+}
